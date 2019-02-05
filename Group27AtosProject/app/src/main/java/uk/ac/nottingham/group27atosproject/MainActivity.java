8077f7f15d1b14ab03745826a2f7722dc1031ccf
David Mares <32355403+ka5p3rr@users.noreply.github.com>
2019-02-05 16:05:26 +0000

    Initial Android App creation
    I created the first log in screen with one button that opens the second activity. So far no additional functionality implemented.
    
    Co-Authored-By: Hugo-Le-Gurun <33239878+Hugo-Le-Gurun@users.noreply.github.com>

---
+++ Group27AtosProject/app/src/main/java/uk/ac/nottingham/group27atosproject/MainActivity.java
@@ -0,0 +1,20 @@
+package uk.ac.nottingham.group27atosproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void launchActivity(View view) {
+        Intent intent = new Intent(this, AppActivity.class);
+        startActivity(intent);
+    }
+}
